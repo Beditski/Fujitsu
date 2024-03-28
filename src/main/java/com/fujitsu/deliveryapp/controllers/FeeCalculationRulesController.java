5be922381caec85d198dc162bf2f4ff30dc51b97
Beditski <a.beditski@gmail.com>
2024-03-28 21:17:59 +0200

    #17 Previous commit was made from the wrong directory. Recommiting.

--- src/main/java/com/fujitsu/deliveryapp/controllers/FeeCalculationRulesController.java
+++ src/main/java/com/fujitsu/deliveryapp/controllers/FeeCalculationRulesController.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
